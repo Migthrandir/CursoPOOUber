a07a9706794b691bbe752039982ff8ed1210eb89
Miguel Araya <maaharaya@gmail.com>
2022-04-26 14:21:11 -0400

    a la par de la clase 25

--- PHP/car.php
+++ PHP/car.php
@@ -4,5 +4,10 @@ class Car{
     public $license;
     public $driver;
     public $passenger;
+
+    public function __construct($license, $driver){
+        $this-> license = $license;
+        $this-> driver = $driver;
     }
+    echo "license: $this->license, conductor: {$this->driver->name}, document: {$this->driver->document}";
 ?>
