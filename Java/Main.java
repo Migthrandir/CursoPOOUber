e08bf7ed5ba632344a309b4d0dba3f60e6003d38
Miguel Araya <maaharaya@gmail.com>
2022-04-26 11:59:47 -0400

    Actualizado el codigo siguiendo la clase 22 del curso

--- Java/Main.java
+++ Java/Main.java
@@ -8,9 +8,15 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car();
         car.license = "AMQ123";
+        car.driver = "Andres Herrera";
         car.passenger = 4;
-        car.driver = "Andres";
-        
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }
 
