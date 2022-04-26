df7050cbd5b286561a1511263b9a8bf101112b08
Miguel Araya <maaharaya@gmail.com>
2022-04-26 11:53:35 -0400

    Commint inicial

---
+++ Java/Main.java
@@ -0,0 +1,17 @@
+package Java;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.passenger = 4;
+        car.driver = "Andres";
+        
+    }
+}
+
+    
